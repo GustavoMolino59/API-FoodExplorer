007dc05905de4c9236e6d717a6d71deadb6a8262
Gustavomolino59 <feodpv02858@odontoprev.com.br>
2024-04-04 20:02:59 -0300

    Alteração na validated

--- src/controller/UserValidatedController.js
+++ src/controller/UserValidatedController.js
@@ -5,15 +5,18 @@ class UsersValidatedController {
 
     async index(request, response) {
         
-        const { user } = request;
-        console.log('user na validated')
-        console.log(user)
-        const checkUserExists = await knex("users").where({ id: user.id });
-        console.log(checkUserExists)
-        if(checkUserExists.length === 0) {
-            throw new AppError("Unauthorized", 401);
+        if(request.user){
+            const { user } = request;
+            console.log('user na validated')
+            console.log(user)
+            const checkUserExists = await knex("users").where({ id: user.id });
+            console.log(checkUserExists)
+            if(checkUserExists.length === 0) {
+                throw new AppError("Unauthorized", 401);
+            }
+            return response.status(200).json();
         }
-        return response.status(200).json();
+        throw new AppError("Unauthorized", 401)
     }
 }
 
